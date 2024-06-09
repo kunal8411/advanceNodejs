e1bdcfd1ed6c77e407e05de4277a4da098c460b0
Kunal Khairnar <kkhairnar@nexuscognitive.com>
2024-06-09 12:28:22 +0530

    Node + React code setup with routes and login setup, setup the backend node js server on the local mongodb

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
+
 require('./models/User');
 require('./models/Blog');
 require('./services/passport');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(keys.mongoURI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(keys.mongoURI);
+
 
 const app = express();
 
